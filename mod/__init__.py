6632ebac5132a01cee55666c557d1f4b04575626
Vanmixer <tvvanmix@gmail.com>
2022-10-23 17:25:07 +0300

    Added post requests for user, category and records

--- mod/__init__.py
+++ mod/__init__.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
 import requests
 
 app = Flask(__name__)
@@ -11,6 +11,23 @@ records = [[1, 1, 1, "20.10.2022", 100],
            [2, 1, 1, "20.10.2022", 100],
            [1, 2, 1, "20.10.2022", 100],
            [1, 7, 1, "20.10.2022", 100]]
+@app.post('/user')
+def create_user():
+    new_one = request.json
+    users.append(new_one)
+    return users
+
+@app.post('/category')
+def create_category():
+    new_one = request.json
+    categories.append(new_one)
+    return categories
+
+@app.post('/record')
+def create_record():
+    new_one = request.json
+    records.append(new_one)
+    return records
 
 class Main(Resource):
     def get(self, user_id):
